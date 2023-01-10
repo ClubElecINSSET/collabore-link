c56b30fcf33de31d06b74a2cea734d20849384c2
Gaëtan L. H.-F <gaetanlhf@noreply.forge.collabore.fr>
2023-01-10 17:32:40 +0100

    Update reported.php

--- reported.php
+++ reported.php
@@ -74,6 +74,12 @@ if (
                     "We will take care of your report as soon as possible"
                 ); ?>
             </p>
+            <a class="btn btn-outline-primary btn-block" type="button" href="/">
+      <span class="bi-fix"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill" viewBox="0 0 16 16" aria-hidden="true">
+  <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+</svg> <?php $translate->__("Return to the homepage"); ?></span>
+      
+    </a>
         </main>
     <?php
     } else {
